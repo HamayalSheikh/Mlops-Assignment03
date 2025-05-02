97d57f627e11ad3a44f49b64d47f8436dd7fcbb3
HamayalSheikh <hamyl.sheikh90@gmail.com>
2025-05-02 23:26:11 +0500

    minor changes

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -17,13 +17,25 @@ import os
 # df.dropna(inplace=True)
 # Create the processed directory if it doesn't exist
 os.makedirs('/opt/airflow/data/processed', exist_ok=True)
-df = pd.read_csv('/opt/airflow/data/raw/raw_data.csv')
+df = pd.read_csv('data/raw/raw_data.csv')
+
+# df = pd.read_csv('/opt/airflow/data/raw/raw_data.csv')
 df.dropna(inplace=True)
 # df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
 
 
 scaler = StandardScaler()
 df[['temperature', 'humidity', 'wind_speed']] = scaler.fit_transform(df[['temperature', 'humidity', 'wind_speed']])
-# df.to_csv('data/processed/processed_data.csv', index=False)
-df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
+df.to_csv('data/processed/processed_data.csv', index=False)
+# df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
 print("✅ Preprocessing complete.")
+
+# import os
+# import platform
+
+# if platform.system() == 'Windows':
+#     data_path = os.path.join('data', 'raw', 'raw_data.csv')
+# else:
+#     data_path = '/opt/airflow/data/raw/raw_data.csv'
+
+# df = pd.read_csv(data_path)
