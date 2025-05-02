f3d4c343a3bd37199db3d594e427d55f0ad1dcc8
HamayalSheikh <hamyl.sheikh90@gmail.com>
2025-05-02 17:52:30 +0500

    Add preprocessed data

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,4 +1,14 @@
-# scripts/preprocess_data.py
+# # scripts/preprocess_data.py
+# import pandas as pd
+# from sklearn.preprocessing import StandardScaler
+
+# df = pd.read_csv('data/raw/raw_data.csv')
+# df.dropna(inplace=True)
+
+# scaler = StandardScaler()
+# df[['temperature', 'humidity', 'wind_speed']] = scaler.fit_transform(df[['temperature', 'humidity', 'wind_speed']])
+# df.to_csv('data/processed/processed_data.csv', index=False)
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
