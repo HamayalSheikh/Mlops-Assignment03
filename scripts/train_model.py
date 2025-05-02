7383048f29bc54c6c217e7142f26b490acc7d0b6
HamayalSheikh <hamyl.sheikh90@gmail.com>
2025-05-02 23:01:28 +0500

    Trained linear regression model

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
+import os
 
 df = pd.read_csv('data/processed/processed_data.csv')
 X = df[['humidity', 'wind_speed']]  # features
@@ -9,5 +10,7 @@ y = df['temperature']               # target
 
 model = LinearRegression().fit(X, y)
 
+# Save the model
+os.makedirs('models', exist_ok=True)
 with open('models/model.pkl', 'wb') as f:
     pickle.dump(model, f)
