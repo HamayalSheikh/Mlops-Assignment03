c8a24015bc260af40f56fc3721ea2cb68456a526
HamayalSheikh <hamyl.sheikh90@gmail.com>
2025-05-10 22:11:13 +0500

    minor change

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -13,22 +13,23 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import os
 
-df = pd.read_csv('data/raw/raw_data.csv')
+# df = pd.read_csv('data/raw/raw_data.csv')
 # df.dropna(inplace=True)
 # Create the processed directory if it doesn't exist
 os.makedirs('/opt/airflow/data/processed', exist_ok=True)
-df = pd.read_csv('data/raw/raw_data.csv')
+# df = pd.read_csv('data/raw/raw_data.csv')
 
-# df = pd.read_csv('/opt/airflow/data/raw/raw_data.csv')
+df = pd.read_csv('/opt/airflow/data/raw/raw_data.csv')
 df.dropna(inplace=True)
-# df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
+df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
 
 
 scaler = StandardScaler()
 df[['temperature', 'humidity', 'wind_speed']] = scaler.fit_transform(df[['temperature', 'humidity', 'wind_speed']])
-df.to_csv('data/processed/processed_data.csv', index=False)
-# df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
+# df.to_csv('data/processed/processed_data.csv', index=False)
+df.to_csv('/opt/airflow/data/processed/processed_data.csv', index=False)
 print("✅ Preprocessing complete.")
+
 
 # import os
 # import platform
